04fd516323fb9b32d22f830c3ee65da0d3ec01fb
bbigal90 <bbigal90@gmail.com>
2021-06-27 21:31:27 +0900

    20210627 21:31 테스트 및 spring-data.xml 수정

--- Final_ERP/src/main/java/com/srcelite/finalerp/web/mvc/NoticeLogic.java
+++ Final_ERP/src/main/java/com/srcelite/finalerp/web/mvc/NoticeLogic.java
@@ -3,7 +3,10 @@ package com.srcelite.finalerp.web.mvc;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
 public class NoticeLogic {
+	Logger logger = Logger.getLogger(NoticeLogic.class);
 	NoticeDao noticeDao = null;
 
 	public void setNoticeDao(NoticeDao noticeDao) {
@@ -12,7 +15,12 @@ public class NoticeLogic {
 	// 중요 공지 사항 조회
 	public List<Map<String,Object>> getMainNoticeList(Map<String,Object> pmap)   {return null;}
 	// 모든 공지 리스트 조회
-	public List<Map<String,Object>> getAllNoticeList(Map<String,Object> pmap)    {return null;}
+	public List<Map<String, Object>> getAllNoticeList(Map<String, Object> pmap) {
+		logger.info("NoticeLogic == getAllNoticeList == 호출");
+		List<Map<String, Object>> noticeAllList = null;
+		noticeAllList = noticeDao.getAllNoticeList(pmap);
+		return noticeAllList;
+	}
 	// 공지 상세조회
 	public List<Map<String,Object>> getDetailNotice(Map<String,Object> pmap)   	 {return null;}
 	// 공지 추가
