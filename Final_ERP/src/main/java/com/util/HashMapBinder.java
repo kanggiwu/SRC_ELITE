3be0851c19ae7130ba32811961c6674b11b6cbb4
bbigal90 <bbigal90@gmail.com>
2021-07-02 22:47:00 +0900

    [210702 2247 임동혁] 위에 파일과 같습니다. 	modified:   src/main/java/com/util/HashMapBinder.java

--- Final_ERP/src/main/java/com/util/HashMapBinder.java
+++ Final_ERP/src/main/java/com/util/HashMapBinder.java
@@ -27,10 +27,10 @@ public class HashMapBinder {
 	public HashMapBinder(HttpServletRequest request) {
 		this.request = request;
 //		realFolder = "http:\\localhost:6001\\pds";
-		realFolder = "C:\\SRC_ELITE\\Final_ERP\\src\\main\\webapp\\pds";
-		myFolder = "/pds";
+// 		realFolder = "C:\\SRC_ELITE\\Final_ERP\\src\\main\\webapp\\pds";
 //		realFolder = "C:\\portfolio_kosmo\\lab_spring4\\spring4_1_1\\WebContent\\pds";
-//		realFolder = "C:\\bbigal_programing\\workspace_web\\SRC_ELITE\\Final_ERP\\src\\main\\webapp\\pds";
+		  realFolder = "C:\\bbigal_programing\\workspace_web\\SRC_ELITE\\Final_ERP\\src\\main\\webapp\\pds";
+		myFolder = "/pds";
 	}
 	public void bind(Map<String,Object> target) {
 		Enumeration en = request.getParameterNames();//배열 구조체 묶음
